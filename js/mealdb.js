805ed40678c5915b8802ef8a0f9cbf22a55119ea
Murad Sarker <sarker24@gmail.com>
2022-02-25 10:01:46 +0100

    Add the featch function in mealdb

--- js/mealdb.js
+++ js/mealdb.js
@@ -12,5 +12,17 @@ loadMeal();
 const displayMeal = (items) => {
   for (const item of items) {
     console.log(item);
+    const mealContainer = document.getElementById("meal-container");
+    const div = document.createElement("div");
+    div.setAttribute("class", "mx-auto");
+    div.setAttribute("id", "bgColor");
+
+    div.innerHTML = `
+    <img class="h-64 w-64 my-2 mx-auto"  src=${item.strMealThumb} alt="">
+    <h3 class="mx-3 ">${item.strMeal}</h3>
+    <p class="mx-3 mb-3">${item.strInstructions.slice(0, 100)}...</p>
+    `;
+
+    mealContainer.appendChild(div);
   }
 };
